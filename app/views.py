d47e3d449a70273a2ff6845cca0abe9ce33fe1cd
Matt Parrilla <matthew.parrilla@gmail.com>
2014-01-30 17:50:50 -0500

    Make river template

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from index import app
 from flask import render_template, request
 from config import BASE_URL
+from query import api_feed
 
 
 @app.route('/')
 def index():
     page_url = BASE_URL + request.path
     page_title = 'VPR App Template'
+    stories = api_feed([262757810], numResults=10)
 
     social = {
         'title': "",
@@ -20,4 +22,5 @@ def index():
     return render_template('content.html',
         page_title=page_title,
         social=social,
+        stories=stories,
         page_url=page_url)
