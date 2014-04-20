bdfe991eade9aa3fd02e5e3ac6c38f208b6c99ac
Matt Parrilla <matthew.parrilla@gmail.com>
2014-04-20 10:44:42 -0400

    Add ability to read google sheets

--- app/query.py
+++ app/query.py
@@ -8,7 +8,29 @@ import os
 from bs4 import BeautifulSoup as Soup
 from datetime import datetime
 from cStringIO import StringIO
-from config import NPR_API_KEY, ABSOLUTE_PATH
+from google_spreadsheet.api import SpreadsheetAPI
+from config import NPR_API_KEY, ABSOLUTE_PATH, GOOGLE_SPREADSHEET
+
+
+def list_sheets():
+    """The API sheet_key is not the same as the key in the URL. This function
+    just prints out all sheet keys"""
+    api = SpreadsheetAPI(GOOGLE_SPREADSHEET['USER'],
+        GOOGLE_SPREADSHEET['PASSWORD'],
+        GOOGLE_SPREADSHEET['SOURCE'])
+    spreadsheets = api.list_spreadsheets()
+    for sheet in spreadsheets:
+        print sheet
+
+
+def get_google_sheet(sheet_key=False, sheet_id='od6'):
+    """Uses python_google_spreadsheet API to interact with sheet"""
+    api = SpreadsheetAPI(GOOGLE_SPREADSHEET['USER'],
+        GOOGLE_SPREADSHEET['PASSWORD'],
+        GOOGLE_SPREADSHEET['SOURCE'])
+    sheet = api.get_worksheet(sheet_key, sheet_id)
+    sheet_object = sheet.get_rows()
+    return sheet_object
 
 
 def api_feed(tag, numResults=1, char_limit=240, thumbnail=False):
