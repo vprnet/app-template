b989041041ab314625e4bd708bc8903a5bfd4c56
Matt Parrilla <matthew.parrilla@gmail.com>
2014-08-16 13:35:47 -0400

    Add call to get worksheet IDs

--- app/query.py
+++ app/query.py
@@ -12,15 +12,19 @@ from google_spreadsheet.api import SpreadsheetAPI
 from config import NPR_API_KEY, ABSOLUTE_PATH, GOOGLE_SPREADSHEET
 
 
-def list_sheets():
+def list_sheets(key=False):
     """The API sheet_key is not the same as the key in the URL. This function
     just prints out all sheet keys"""
     api = SpreadsheetAPI(GOOGLE_SPREADSHEET['USER'],
         GOOGLE_SPREADSHEET['PASSWORD'],
         GOOGLE_SPREADSHEET['SOURCE'])
     spreadsheets = api.list_spreadsheets()
-    for sheet in spreadsheets:
-        print sheet
+    if key:
+        worksheets = api.list_worksheets(key)
+        print worksheets
+    else:
+        for sheet in spreadsheets:
+            print sheet
 
 
 def get_google_sheet(sheet_key=False, sheet_id='od6'):
