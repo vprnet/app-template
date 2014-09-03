392224b8a4cb3890f609ffb74f8e8a84b15a7dad
Matt Parrilla <matthew.parrilla@gmail.com>
2014-09-03 15:21:41 -0400

    Remove webfaction middleware

--- main/__init__.py
+++ main/__init__.py
@@ -5,16 +5,5 @@ app.config.from_object('config')
 
 from main import views
 
-
-class WebFactionMiddleware(object):
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        environ['SCRIPT_NAME'] = '/template'
-        return self.app(environ, start_response)
-
-app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
-
 if __name__ == '__main__':
     app.run()
