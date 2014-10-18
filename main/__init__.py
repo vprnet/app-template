b47cb8fdc802c0a4b5c7c83e15bd114b12332a0c
Matt Parrilla <matthew.parrilla@gmail.com>
2014-10-18 10:07:47 -0400

    Fix config path

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import WEBFACTION_PATH
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('main.config')
 
 from main import views
 
