e7ad2d95d3bbcc399e82aa3e55097d859b3fa82b
Matt Parrilla <matthew.parrilla@gmail.com>
2014-10-18 10:05:52 -0400

    Add debug by to default

--- runserver.py
+++ runserver.py
@@ -6,6 +6,8 @@ from main import app
 from upload_s3 import set_metadata
 from flask_frozen import Freezer
 
+app.debug = True
+
 if len(sys.argv) > 1 and sys.argv[1] == 'freeze':
     freezer = Freezer(app)
     freezer.freeze()
